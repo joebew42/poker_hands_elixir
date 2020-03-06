254e757d5ba5d51bedd4c3e924b3c0fb33844381
JoeBew42 <joebew42@gmail.com>
2020-03-06 13:33:10 +0100

    remove the fake implementation with a real one

--- lib/hand_rank.ex
+++ lib/hand_rank.ex
@@ -11,6 +11,12 @@ defmodule HandRank do
   end
 
   defp highest_card_from(cards) do
-    %Card{suit: :clubs, rank: 6}
+    cards
+    |> Enum.reduce(fn card, current_highest_card ->
+      case Card.greater_than?(card, current_highest_card) do
+        true -> card
+        false -> current_highest_card
+      end
+    end)
   end
 end

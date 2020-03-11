d6a953ba4e788dc3b7a93254043a6bf0c47799db
JoeBew42 <joebew42@gmail.com>
2020-03-11 15:17:13 +0100

    refine the dsl for test

--- test/hand_rank_test.exs
+++ test/hand_rank_test.exs
@@ -2,7 +2,7 @@ defmodule HandRankTest do
   use ExUnit.Case, async: true
 
   describe "when a hand has one pair" do
-    test "returns the two cards of one suit" do
+    test "returns the two cards with the same rank" do
       hand =
         Hand.with([
           Card.clubs_of(2),
@@ -12,9 +12,7 @@ defmodule HandRankTest do
           Card.clubs_of(6)
         ])
 
-      assert one_pair_rank?(HandRank.of(hand), with: [
-        Card.clubs_of(2), Card.diamonds_of(2)
-      ])
+      assert one_pair_rank?(hand, with: [Card.clubs_of(2), Card.diamonds_of(2)])
     end
   end
 
@@ -29,15 +27,15 @@ defmodule HandRankTest do
           Card.clubs_of(6)
         ])
 
-      assert high_card_rank?(HandRank.of(hand), with: Card.clubs_of(6))
+      assert high_card_rank?(hand, with: Card.clubs_of(6))
     end
   end
 
-  defp high_card_rank?(hand_rank, with: card) do
-    hand_rank == %HandRank{rank: :high_card, cards: [card]}
+  defp high_card_rank?(hand, with: card) do
+    HandRank.of(hand) == %HandRank{rank: :high_card, cards: [card]}
   end
 
-  defp one_pair_rank?(hand_rank, with: [first_card, second_card]) do
-    hand_rank == %HandRank{rank: :one_pair, cards: [first_card, second_card]}
+  defp one_pair_rank?(hand, with: [first_card, second_card]) do
+    HandRank.of(hand) == %HandRank{rank: :one_pair, cards: [first_card, second_card]}
   end
 end
